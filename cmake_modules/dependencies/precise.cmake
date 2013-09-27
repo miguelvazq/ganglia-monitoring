b1700fbf0cacf0e0fcdf7246f380274e5a2739fe
Gleb Aronsky <gleb.aronsky@lexisnexis.com>
2013-09-27 17:13:36 -0400

    Added script (work in progress) to automate install to VM fix dependency fix cronjob
    Signed-off-by: Gleb Aronsky <gleb.aronsky@lexisnexis.com>

--- cmake_modules/dependencies/precise.cmake
+++ cmake_modules/dependencies/precise.cmake
@@ -1,1 +1,1 @@
-set ( CPACK_DEBIAN_PACKAGE_DEPENDS  "hpccsystems-platform, collectd-core, ganglia-monitor python-lxml")
+set ( CPACK_DEBIAN_PACKAGE_DEPENDS  "hpccsystems-platform, collectd-core, ganglia-monitor, python-lxml")
