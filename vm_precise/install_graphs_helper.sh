34f7c82fc20892e295d5d7c50aaaa1e1b51a9210
Gleb Aronsky <gleb.aronsky@lexisnexis.com>
2013-10-01 10:28:59 -0400

    script
    Signed-off-by: Gleb Aronsky <gleb.aronsky@lexisnexis.com>

--- vm_precise/install_graphs_helper.sh
+++ vm_precise/install_graphs_helper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Installing Ganglia
-apt-get install ganglia-webfrontend gmetad ganglia-monitor python-lxml collectd-core ganglia-monitor python-lxml libltdl7 libganglia1 libapr1 libconfuse0 libxslt1.1 libconfuse-common -y
+# apt-get install ganglia-webfrontend gmetad ganglia-monitor python-lxml collectd-core ganglia-monitor python-lxml libltdl7 libganglia1 libapr1 libconfuse0 libxslt1.1 libconfuse-common -y
 
 # Aliasing Apache web directory to use ganglia-webfrontend
 echo "Alias /ganglia /usr/share/ganglia-webfrontend" >> /etc/apache2/apache2.conf
