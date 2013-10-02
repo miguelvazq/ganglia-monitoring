c64f9b6a2e8bf6367fcdb653cf75ada96896526f
root <root@HPCCSystemsVM-amd64-4.0.2-2>
2013-10-02 15:39:02 +0000

    Update script
    Signed-off-by: Gleb Aronsky <gleb.aronsky@lexisnexis.com>

--- vm_precise/install_graphs_helper.sh
+++ vm_precise/install_graphs_helper.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-# Installing Ganglia
-# apt-get install ganglia-webfrontend gmetad ganglia-monitor python-lxml collectd-core ganglia-monitor python-lxml libltdl7 libganglia1 libapr1 libconfuse0 libxslt1.1 libconfuse-common -y
-
 # Aliasing Apache web directory to use ganglia-webfrontend
 echo "Alias /ganglia /usr/share/ganglia-webfrontend" >> /etc/apache2/apache2.conf
 
 # Customizing demo VM names and labels
 sed 's/my cluster\" localhost/VM Cluster\" localhost/g' < /etc/ganglia/gmetad.conf > /tmp/gmetad.conf; mv /tmp/gmetad.conf /etc/ganglia/gmetad.conf
 echo "gridname \"VM Grid\"" >> /etc/ganglia/gmetad.conf
-
-# Installing HPCC specific settings for gmond data gathering.  This monitors the roxie service and gathers metrics for gmond.
-dpkg -i hpccsystems-ganglia-monitoring--precise_*.deb
 
 # Setting up VM settings to get graphs up and running.  This overwrites many of the installed files.
 cp -f ./conf.php /usr/share/ganglia-webfrontend/
@@ -35,8 +29,6 @@ cp -f ./hpcc_logo.png /usr/share/ganglia-webfrontend/templates/default/images/
 
 # Set gmond to start on reboot
 cp -sf /usr/sbin/gmond /etc/init.d/
-
-
 
 service gmetad stop
 killall gmond
